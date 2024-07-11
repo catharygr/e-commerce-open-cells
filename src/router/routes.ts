d7a8f92eb396885e9c0ee222ce097d7d43001d36
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-11 11:24:40 +0100

    Agregados todos los ficheros

---
+++ src/router/routes.ts
@@ -0,0 +1,12 @@
+import { RouteDefinition } from '@open-cells/core/types';
+
+export const routes: RouteDefinition[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: 'home-page',
+    action: async () => {
+      await import('../pages/home/home-page.js');
+    },
+  },
+];
