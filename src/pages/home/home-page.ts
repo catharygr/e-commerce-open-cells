29a48e0a7c5e63e3ffd950941b2de5ad8ac0ff7b
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-11 11:51:15 +0100

    Instalado json-server

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -2,19 +2,19 @@ import { html, LitElement } from 'lit';
 import { PageController } from '@open-cells/page-controller';
 import { customElement } from 'lit/decorators.js';
 
-// @ts-ignore
 @customElement('home-page')
 export class HomePage extends LitElement {
   pageController = new PageController(this);
 
-  protected createRenderRoot(): HTMLElement | DocumentFragment {
-    // @ts-ignore
-    return this;
-  }
-
   render() {
     return html`
-      <button @click="${() => this.pageController.navigate('second')}">Go to second page</button>
+      <button @click="${() => this.pageController.navigate('second')}">
+        Go to second page
+      </button>
     `;
   }
 }
+// Este código es de open cells
+// protected createRenderRoot(): HTMLElement | DocumentFragment {
+//   return this;
+// }
