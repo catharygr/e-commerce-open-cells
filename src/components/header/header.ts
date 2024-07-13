0621d307ca1882901b516042ca6c173e0832c860
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-13 12:17:47 +0100

    Trabajando en la barra de búsqueda

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,26 +1,46 @@
 import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 import { styles } from './header.css.js';
 import CssReset from '../../css/reset.css.js';
 import '@material/web/icon/icon.js';
 import '@material/web/textfield/filled-text-field';
+import { PageController } from '@open-cells/page-controller';
 
 @customElement('header-component')
 export class HeaderComponent extends LitElement {
+  pageController = new PageController(this);
   static styles = [CssReset, styles];
 
+  @state()
+  private searchValue = '';
+
+  handleSearch(e: Event) {
+    const target = e.target as HTMLInputElement;
+    this.searchValue = target.value;
+  }
+
   render() {
+    console.log(this.searchValue);
     return html` <header>
       <md-icon>storefront</md-icon>
       <nav>
         <ul class="main-nav">
-          <li><a href="/">Inicio</a></li>
+          <li>
+            <a href="/" @click=${this.pageController.navigate('home')}
+              >Inicio</a
+            >
+          </li>
           <li><a href="/categorias">Categorias</a></li>
           <li><a href="/ofertas">Ofertas</a></li>
           <li><a href="/contact">Contacto</a></li>
         </ul>
       </nav>
-      <md-filled-text-field placeholder="Buscar productos" icon="search">
+      <md-filled-text-field
+        placeholder="Buscar productos"
+        icon="search"
+        @input="${(e: Event) => this.handleSearch(e)}"
+        .value=${this.searchValue}
+      >
         <md-icon slot="trailing-icon">search</md-icon>
       </md-filled-text-field>
       <div class="action-header">
