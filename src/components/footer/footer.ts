10915b81784ded0896243107891b0d8c8afbbd66
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-13 11:17:03 +0100

    Trabajando en el css del componente  footer.ts

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,18 +1,23 @@
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import CssReset from '../../css/reset.css.js';
 
 @customElement('footer-component')
 export class FooterComponent extends LitElement {
-  static styles = css`
-    footer {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 1rem;
-      background-color: #f8f8f8;
-      border-top: 1px solid #e0e0e0;
-    }
-  `;
+  static styles = [
+    CssReset,
+    css`
+      footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-block: 2rem;
+        background-color: #f8f8f8;
+        border-top: 1px solid #e0e0e0;
+      }
+    `,
+  ];
+
   render() {
     return html` <footer>
       <p>
