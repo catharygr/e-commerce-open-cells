98b1395cae63b6fbec44e4034ac0e864aabadd95
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-18 17:22:50 +0100

    Trabajando en la ruta de las ofertas

--- src/pages/ofertas/ofertas-page.ts
+++ src/pages/ofertas/ofertas-page.ts
@@ -1,8 +1,14 @@
+// @ts-nocheck
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { PageController } from '@open-cells/page-controller';
+import CssReset from '../../css/reset.css.js';
+import '../../components/Others/spinner.js';
+import '../../components/cards/product-card-small.js';
 
 @customElement('ofertas-page')
 export class OfertasPage extends LitElement {
+  pageController = new PageController(this);
   static styles = css`
     .container {
       display: flex;
@@ -16,7 +22,23 @@ export class OfertasPage extends LitElement {
     }
   `;
 
+  static inbounds = {
+    allOfertas: { channel: 'all-ofertas' },
+  };
+
   render() {
-    return html`<div class="container">Ofertas</div>`;
+    console.log(this.allOfertas);
+    return !this.allOfertas
+      ? html`<spinner-element></spinner-element>`
+      : html` <div class="container">${this.oferCards()}</div> `;
+  }
+
+  oferCards() {
+    const ofertasFiltradas = this.allOfertas.filter(
+      (oferta) => ofertas.offer === true
+    );
+    return ofertasFiltradas.map((oferta) => {
+      return html`<product-card-small .product=${oferta}></product-card-small>`;
+    });
   }
 }
