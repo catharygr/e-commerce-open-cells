30d2bdd9810f67c00de75333fa8f4a5a6f12459d
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-18 14:14:03 +0100

    Trabajando en home-hage.ts

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { html, LitElement, css } from 'lit';
 import { PageController } from '@open-cells/page-controller';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 import '../../components/cards/home-card.js';
 import '../../components/Others/spinner.js';
 
@@ -19,18 +19,21 @@ export class HomePage extends LitElement {
     }
   `;
 
+  @state()
+  randomProduct = null;
+
+  getRandomProduct() {
+    if (!this.allProducts) return;
+    const randomNum = Math.floor(Math.random() * this.allProducts.length);
+    this.randomProduct = this.allProducts[randomNum];
+  }
+
   static inbounds = {
-    randomProduct: {
-      channel: 'all-products',
-      action: (data) => {
-        if (!data) return;
-        const randomNum = Math.floor(Math.random() * data.length);
-        return data[randomNum];
-      },
-    },
+    allProducts: { channel: 'all-products' },
   };
 
   render() {
+    this.getRandomProduct();
     return !this.randomProduct
       ? html`<spinner-element></spinner-element>`
       : html`
@@ -41,5 +44,7 @@ export class HomePage extends LitElement {
         `;
   }
 
-  onPageEnter() {}
+  onPageEnter() {
+    this.randomProduct = this.getRandomProduct();
+  }
 }
