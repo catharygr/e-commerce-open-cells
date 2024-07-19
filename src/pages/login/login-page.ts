150766039b023b5a5361198852ea11f046ae2aa1
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-19 13:11:52 +0100

    Trabajando en el componente login-page.ts

--- src/pages/login/login-page.ts
+++ src/pages/login/login-page.ts
@@ -1,10 +1,29 @@
 // @ts-nocheck
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import '@material/web/textfield/outlined-text-field';
+import '@material/web/button/filled-button.js';
 
 @customElement('login-page')
 export class LoginPage extends LitElement {
   render() {
-    return html`<h1>My login</h1>`;
+    return html`<h1>My login</h1>
+      <form>
+        <md-outlined-text-field
+          label="Email"
+          id="email"
+          name="email"
+          type="email"
+          required
+        ></md-outlined-text-field>
+        <md-outlined-text-field
+          label="Password"
+          id="password"
+          name="password"
+          type="password"
+          required
+        ></md-outlined-text-field>
+        <md-filled-button type="submit">Login</md-filled-button>
+      </form>`;
   }
 }
