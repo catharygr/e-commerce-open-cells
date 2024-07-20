e3817971a11222eb81f0be109f673bf01076df10
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-20 13:30:45 +0100

    Terminada la función handleLogOff()

--- src/pages/account/account-page.ts
+++ src/pages/account/account-page.ts
@@ -3,9 +3,12 @@ import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import CssReset from '../../css/reset.css.js';
 import '@material/web/button/filled-button.js';
+import { PageController } from '@open-cells/page-controller';
+import { resetInterceptorContext } from '@open-cells/core/src/bridge.js';
 
 @customElement('account-page')
 export class AccountPage extends LitElement {
+  pageController = new PageController(this);
   static styles = [
     CssReset,
     css`
@@ -18,14 +21,29 @@ export class AccountPage extends LitElement {
         padding: 1rem;
         border-radius: 0.5rem;
       }
+      .acc-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
     `,
   ];
+
+  handleLogOff() {
+    sessionStorage.removeItem('user');
+    resetInterceptorContext();
+    this.pageController.navigate('home');
+  }
 
   render() {
     return html`
       <div class="container">
-        <h1>My account</h1>
-        <md-filled-button>Logout</md-filled-button>
+        <div class="acc-header">
+          <h1>My account</h1>
+          <md-filled-button @click=${this.handleLogOff} class=""
+            >Logout</md-filled-button
+          >
+        </div>
       </div>
     `;
   }
