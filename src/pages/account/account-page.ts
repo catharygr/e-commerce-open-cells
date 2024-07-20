e22b22e435c8ee2a666a429f290ac85d12692c26
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-20 13:13:21 +0100

    Trabajando en el css del  componente account-page.ts

--- src/pages/account/account-page.ts
+++ src/pages/account/account-page.ts
@@ -2,19 +2,31 @@
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import CssReset from '../../css/reset.css.js';
+import '@material/web/button/filled-button.js';
 
 @customElement('account-page')
 export class AccountPage extends LitElement {
   static styles = [
     CssReset,
     css`
-      h1 {
-        text-align: center;
+      :host {
+        max-width: 50rem;
+        margin: 1rem auto;
+      }
+      .container {
+        border: 1px solid #ccc;
+        padding: 1rem;
+        border-radius: 0.5rem;
       }
     `,
   ];
 
   render() {
-    return html`<h1>My account</h1>`;
+    return html`
+      <div class="container">
+        <h1>My account</h1>
+        <md-filled-button>Logout</md-filled-button>
+      </div>
+    `;
   }
 }
