f8c457f0a11af16d323f442bc5ce6d430e8d6ed8
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-21 17:53:50 +0100

    Trabajando en el edit-page.ts

--- src/pages/edit/edit-page.ts
+++ src/pages/edit/edit-page.ts
@@ -2,6 +2,7 @@ import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import CssReset from '../../css/reset.css.js';
 import { PageController } from '@open-cells/page-controller';
+import '@material/web/button/filled-button.js';
 
 @customElement('edit-page')
 export class EditPage extends LitElement {
@@ -21,7 +22,9 @@ export class EditPage extends LitElement {
     console.log('Edit Page');
     return html`
       <h1>Edit Page</h1>
-      <p>This is the edit page</p>
+      <md-filled-button @click=${() => this.pageController.backStep()}
+        >Go to Admin</md-filled-button
+      >
     `;
   }
 }
