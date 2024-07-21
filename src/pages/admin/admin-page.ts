2846243a03c31fe4cd03522ed2a39db3d8bcebd9
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-21 17:29:53 +0100

    Trabajando en el admin-page.ts

--- src/pages/admin/admin-page.ts
+++ src/pages/admin/admin-page.ts
@@ -3,6 +3,7 @@ import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import CssReset from '../../css/reset.css.js';
 import { PageController } from '@open-cells/page-controller';
+import '@material/web/button/filled-button.js';
 
 @customElement('admin-page')
 export class AdminPage extends LitElement {
@@ -23,14 +24,50 @@ export class AdminPage extends LitElement {
         padding: 1rem;
         border-radius: 0.5rem;
       }
+      .admin-edit {
+        display: flex;
+        flex-direction: column;
+        gap: 0.3rem;
+
+        & > :nth-child(odd) {
+          background-color: #b9b9b9;
+        }
+      }
+      .card-product {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+        border: 1px solid #ccc;
+        border-radius: 0.5rem;
       }
     `,
   ];
+
+  static inbounds = {
+    allProducts: { channel: 'all-products' },
+  };
 
   render() {
     return html`<div class="container">
       <h1>Admin</h1>
       <p>Puedes editar en produtos que ya existen o cargar uno nuevo</p>
+      <div class="admin-edit">
+        ${this.allProducts?.map(
+          (product) =>
+            html`<div class="card-product">
+              <h3>${product.title}</h3>
+              <md-filled-button
+                class="edit-button"
+                @click=${() =>
+                  this.pageController.navigate('editar-producto', {
+                    id: product.id,
+                  })}
+                >Editar</md-filled-button
+              >
+            </div>`
+        )}
+      </div>
     </div>`;
   }
 }
