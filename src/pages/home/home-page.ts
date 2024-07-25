4efee84852033ab2da6170251239f6b279aff608
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-25 16:01:51 +0100

    Trabajando en el componente home-page.ts

--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -20,60 +20,16 @@ export class HomePage extends LitElement {
         margin-top: 0;
         text-align: center;
       }
-      .carousel {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-      .carousel-inner {
-        display: flex;
-        overflow: hidden;
-        width: 300px;
-        margin: 0 auto;
-      }
-      .carousel-item {
-        min-width: 100%;
-        transition: transform 0.5s ease-in-out;
-      }
-      .carousel-buttons {
-        display: flex;
-        justify-content: space-between;
-        width: 300px;
-        margin: 0 auto;
-      }
-      button {
-        background: none;
-        border: none;
-        cursor: pointer;
-        font-size: 1.5rem;
-      }
     `,
   ];
 
   @state()
   randomProduct = null;
 
-  @state()
-  currentIndex = 0;
-
-  connectedCallback() {
-    super.connectedCallback();
-    this.getRandomProducts();
-  }
-
   getRandomProduct() {
     if (!this.allProducts) return;
     const randomNum = Math.floor(Math.random() * this.allProducts.length);
     this.randomProduct = this.allProducts[randomNum];
-  }
-
-  next() {
-    this.currentIndex = (this.currentIndex + 1) % this.products.length;
-  }
-
-  prev() {
-    this.currentIndex =
-      (this.currentIndex - 1 + this.products.length) % this.products.length;
   }
 
   static inbounds = {
