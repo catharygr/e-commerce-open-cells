fc0a1d705489fbfc74974fceb30310b0dffb0374
catharygr <103951447+catharygr@users.noreply.github.com>
2024-07-31 13:00:43 +0100

    Trabajando en la lógica del componente shopping-cart.ts

--- src/components/shopping-cart/shopping-cart.ts
+++ src/components/shopping-cart/shopping-cart.ts
@@ -1,26 +1,40 @@
 // @ts-nocheck
 import { LitElement, html, css } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
+import { ElementController } from '@open-cells/element-controller';
 
 @customElement('shopping-cart')
 export class ShoppingCart extends LitElement {
+  elementController = new ElementController(this);
   @state()
-  productsInCart = [3, 6, 9, 9, 9];
+  productsInCart = [];
 
   static inbounds = {
     allProducts: { channel: 'all-products' },
     userState: { channel: 'user-state' },
   };
 
+  findProductsInCart() {
+    // const products = this.userState?.cart;
+    const productsID = [3, 6, 9, 9, 9];
+    this.productsInCart = productsID?.map((id) => {
+      return this.allProducts?.find((product) => product.id === id);
+    });
+    console.log(this.productsInCart);
+  }
+
   render() {
-    return html`
-      <div>
-        <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
-        </ul>
-      </div>
-    `;
+    this.findProductsInCart();
+    return !this.allProducts
+      ? html`<spinner-element></spinner-element>`
+      : html`
+          <div>
+            <ul>
+              <li>Product 1</li>
+              <li>Product 2</li>
+              <li>Product 3</li>
+            </ul>
+          </div>
+        `;
   }
 }
