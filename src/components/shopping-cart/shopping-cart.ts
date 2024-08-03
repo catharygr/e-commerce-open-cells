86ec0fb9f8fa6059246757612aeaffb986f93a53
catharygr <103951447+catharygr@users.noreply.github.com>
2024-08-03 14:09:36 +0100

    Trabajando en la lógica del componente shopping-cart.ts

--- src/components/shopping-cart/shopping-cart.ts
+++ src/components/shopping-cart/shopping-cart.ts
@@ -58,15 +58,15 @@ export class ShoppingCart extends LitElement {
     userState: { channel: 'user-state' },
   };
 
-  findProductsInCart() {
-    // const products = this.userState?.cart;
-    const productsID = [3, 6, 9];
-    return productsID?.map((id) => {
-      return this.allProducts?.find(
-        (product) => product.id.toString() === id.toString()
-      );
-    });
-  }
+  // findProductsInCart() {
+  //   // const products = this.userState?.cart;
+  //   const productsID = [3, 6, 9];
+  //   return productsID?.map((id) => {
+  //     return this.allProducts?.find(
+  //       (product) => product.id.toString() === id.toString()
+  //     );
+  //   });
+  // }
 
   render() {
     const productTemplate = html`
